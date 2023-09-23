f59116cb0955f1feb10f1691878b2e8bd26a222f
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2023-09-23 10:58:44 +0300

    fix: rounded cant worked with size

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -17,9 +17,9 @@ const buttonVariants = cva("inline-flex items-center justify-center text-sm font
     },
     size: {
       default: "h-10 px-4 py-2",
-      sm: "h-9 rounded-md px-3",
-      md: "h-10 rounded-md px-4",
-      lg: "h-11 rounded-md px-8",
+      sm: "h-9 px-3",
+      md: "h-10 px-4",
+      lg: "h-11 px-8",
       icons: "h-10 w-10",
     },
     rounded: {
